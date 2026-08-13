0f8d500ccdac2144f69f7bac5851c334391f4252
hezhxp <hcalub87@gmail.com>
2026-08-13 23:56:05 +0100

    Walking and Idle Animation
    Animation connected to Player, IsWalking false when Idle and true when Walking

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+
+    private bool isWalking = false;
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -28,11 +30,18 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotateSpeed = 10f;
 
         transform.position += moveDir * moveSpeed * Time.deltaTime;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
         Debug.Log(Time.deltaTime);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
 
