10565684704fa169ece8e1c9931c1d170b5451b8
hezhxp <hcalub87@gmail.com>
2026-08-15 22:56:55 +0100

    Refactor GameMovement
    Forgot to remove input code in player, should be refactored now

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,24 +9,6 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
