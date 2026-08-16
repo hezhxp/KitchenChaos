7cbbe3efe9a1354e1488c1c0d15ed8a5d5367ff2
hezhxp <hcalub87@gmail.com>
2026-08-17 00:00:19 +0100

    Collisions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,11 +12,17 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
+        float playerSize = .7f;
+        bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
+
         isWalking = moveDir != Vector3.zero;
 
         float rotateSpeed = 10f;
-
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
         Debug.Log(Time.deltaTime);
     }
