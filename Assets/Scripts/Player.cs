a6cb20548fb7869b806593e237f05799feb0ac85
hezhxp <hcalub87@gmail.com>
2026-08-17 22:43:06 +0100

    Fixed Collision and PlayerMovement
    Collision had a simple ray cast which on fires a thin laser in front of player so if player moves backwards into cube it clips, fixed by having a capsule cast which is basically the laser but incapsulates player at all times = collision fixed
    
    Player movement fixed, if can't move due to a collision, and player is moving diagonally e.i both w and d pressed, then the player would normally just get stuck into the collision but now if both being pressed collision is in front and still wanting to go diagonally, it will actually go to right instead of up as there is a collision - done by if cant move y then can move x

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,12 +12,37 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        float playerSize = .7f;
-        bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = moveDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = moveDir.z != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                } else
+                {
+                    // cannot move in any direction
+                }
+            }
+        }
 
         if (canMove)
         {
-            transform.position += moveDir * moveSpeed * Time.deltaTime;
+            transform.position += moveDir * moveDistance;
         }
 
         isWalking = moveDir != Vector3.zero;
